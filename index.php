0175fbaed45d5a4d3ca796cc5703933e10a2d8dd
Kazan33333 <kazan.kk@gmail.com>
2022-01-02 19:11:32 +0100

    Added config.php, routing.php, database.php

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
-echo 'Hi there 👋 <br>';
-$sth = 'Hello there';
-echo $sth;
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('projects', 'DefaultController');
+Router::post('login', 'SecurityController');
+Router::post('addProject', 'ProjectController');
+
+Router::run($path);
